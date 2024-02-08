f120c855007b56562f32382d205bea121a466fa7
Kronossss <iafsalmeida.cic@uesc.br>
2024-02-08 14:41:08 -0300

    SprinBoot

---
+++ Springboot/redesocial/src/main/java/com/redesocial/redesocial/RedesocialApplication.java
@@ -0,0 +1,16 @@
+package com.redesocial.redesocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class RedesocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedesocialApplication.class, args);
+		System.out.println("penis");
+	}
+
+}
